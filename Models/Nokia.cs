c1e3b3d45b17a6bb68bc51cc2a2e15aea2bdda2b
alesanin1 <alessandro.rosajr@outlook.com>
2024-04-23 12:29:48 -0300

    Projeto_Celulares

---
+++ Models/Nokia.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+
+namespace DesafioPOO.Models
+{
+    
+
+    public class Nokia : Smartphone
+    {
+
+
+        
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} no Nokia");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia=new Nokia("45294042","1","3343",64);
+
+nokia.InstalarAplicativo("WhatsApp");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Smartphone iphone = new Iphone("45294232", "1", "3231", 128);
+
+iphone.InstalarAplicativo("Youtube");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+
